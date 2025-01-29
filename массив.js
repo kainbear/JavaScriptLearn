4d234ce569fd549dddafaaf9c0a9583e8a5233d3
Stanislav <devil_2009@live.ru>
2025-01-29 19:07:02 +0700

    5 commit

---
+++ массив.js
@@ -0,0 +1,12 @@
+// Массив - это объект с цифровыми именами свойств
+
+// Формат записи массивов
+
+const myArray = [1,2,3]
+console.log(myArray)
+
+const myArray2 = new Array(4,5,6)
+console.log(myArray2)
+
+const myArr = [1, true, 'Stas']
+console.log(myArr)
